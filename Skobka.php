e748be36823276b13961096c6da0fa9d9e38f630
mrfanbender <habibullinrustam@yandex.ru>
2018-01-09 01:08:25 +0500

    skobka class

---
+++ Skobka.php
@@ -0,0 +1,34 @@
+<?php
+namespace Habibullin
+class Skobka
+{
+	private $open_sk = 0;
+	private $close_sk = 0;
+	
+	public function compare(String $str){
+		$i=0;
+		$str = preg_replace("/[\t\r\n\s]+/", '', $str);	
+		while(isset($str[$i])){
+			if($str[$i]=='('){
+				$this->open_sk++;
+			}elseif($str[$i]==')'){
+				$this->close_sk++;
+			}else{
+				throw new InvalidArgumentException('Недопустимый символ в строке:'.$str[$i]);			
+			}
+			$i++;
+		}
+		if($this->open_sk == $this->close_sk)
+			return true;
+		return false;
+	} 
+}
+
+$str = "(())";
+$sk_ex = new Skobka();
+try{
+	echo var_dump($sk_ex->compare($str));
+}catch(InvalidArgumentException $e){
+	echo $e->getMessage();
+}
+
